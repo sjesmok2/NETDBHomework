fde1b9505b90687c432e078f2094a953ef238d8a
BIT student <bitstudent@bitnet.wctc.edu>
2020-01-27 20:47:21 -0600

    Add project files.

---
+++ homework1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ticket = "Ticket.cvs";
+            string option;
+            do
+            { 
+                Console.WriteLine("1 | Read ticket information:");
+                Console.WriteLine("2 | Add new ticket information:");
+                Console.WriteLine("Press any key to exit");
+                option = Console.ReadLine();
+
+                if (option == "1")
+                {
+                    if(File.Exists(ticket))
+                    {
+
+                    }
+                }
+            }
+      
+        }
+    }
+}
